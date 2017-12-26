2dee9a45c8e3a009daa58bffddbf682651e6e53c
kumaran-PC\kumaran <veerammalkumaran@gmail.com>
2017-12-27 00:25:16 +0530

    port from env config

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3005);
+server.listen(process.env.PORT || 3005);
 
 app.use(express.static('public'))
 
