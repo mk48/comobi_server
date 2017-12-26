2a68e9752289bb7e79a29dd7983a65c9aeef42b8
kumaran-PC\kumaran <veerammalkumaran@gmail.com>
2017-12-27 00:03:18 +0530

    stage 1 complete

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000);
+server.listen(3005);
+
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
-
 
 io.on('connection',  (socket) => {
   console.log("connected...")
@@ -23,22 +25,31 @@ io.on('connection',  (socket) => {
 
   //----------------------------------------- mobile join ----------------------
   socket.on('mobilejoin',  (data) => {
+    console.log("mob join: " + data.roomid );
     roomid = data.roomid;
     socket.join(data.roomid);
     io.to(data.roomid).emit('mobilejoined');
-    socket.emit('mobilejoined', {roomid : data.roomid})
+    socket.emit('mobilejoined', { "roomid" : data.roomid})
+  });
+
+  //----------------------------------------- closed ---------------------------
+  socket.on('close',  (data) => {
+    console.log("close");
+    io.to(data.roomid).emit('close');
+    socket.leave(data.roomid);
   });
 
   //----------------------------------------- txt ------------------------------
   socket.on('txt',  (data) => {
-    io.to(data.roomid).emit(data.msg);
+    //console.log(data);
+    io.to(data.roomid).emit('txt', data.msg);
   });
 
   //----------------------------------------- disconnect -----------------------
   socket.on('disconnect', () => {
     console.log("..x..")
+    io.to(roomid).emit('close');
     socket.leave(roomid);
-    io.to(roomid).emit('closed');
   });
 
 });
